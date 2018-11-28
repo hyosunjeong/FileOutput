b69e01917008ffdc6c6b5a2c91f9f076d4e33704
hyosunjeong <jeong8161@naver.com>
2018-11-28 16:19:52 +0900

    20181128

---
+++ src/com/biz/printout/Print05.java
@@ -0,0 +1,42 @@
+package com.biz.printout;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class Print05 {
+	
+	/*
+	 * PrintWriter 클래스를 사용해서
+	 * 구구단 5단을 gugudan.txt파일에 작성하시오
+	 * 5 x 1 = 5
+	 * 5 X 2 = 10
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String printFile ="D:/bizwork/workspace/ExFiles/gugudan.txt";
+		
+		
+		try {
+			
+			PrintWriter pw = new PrintWriter(printFile);
+		    int intNum =5;
+			for(int i = 1; i < 10; i++) {
+				pw.println(intNum + "x" + i+ "=" + (intNum*i));
+			//	pw.printf("%d x %d = %d\r\n",5,(i),(5*i));
+			}
+				
+				pw.close();
+				System.out.println("작성 완료");
+				
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("오류");
+		}
+	
+	}
+
+}
